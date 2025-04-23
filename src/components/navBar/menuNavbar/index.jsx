9ea26c8d749cd3c555b69022da9113f50473f5cc
smmu94 <stephaniemmu@gmail.com>
2025-04-23 20:42:50 +0200

    Update navBar component

--- src/components/navBar/menuNavbar/index.jsx
+++ src/components/navBar/menuNavbar/index.jsx
@@ -4,20 +4,23 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { routes } from "@/utils/constants";
 import "@/components/navBar/navBar.scss";
 
-const MenuNavBar = (props) => {
+const MenuNavBar = ({ items }) => {
   const navigate = useNavigate();
   const logout = () => {
     sessionStorage.clear();
     navigate(routes.home);
   };
+
   return (
     <>
-      <NavLink data-testid="link1" to={props.link1}>
-        {props.item1}
-      </NavLink>
-      <NavLink to={props.link2}>{props.item2}</NavLink>
+      {items.map(({ label, path }, index) => (
+        <NavLink data-testid={`link-${index}`} key={path} to={path}>
+          {label}
+        </NavLink>
+      ))}
       <BiLogOut className="logout" data-testid="logout" onClick={logout} />
     </>
   );
 };
+
 export default MenuNavBar;
---
+++ src/views/order/constants.js
@@ -0,0 +1,6 @@
+import { routes } from "@/utils/constants";
+
+export const NAVBAR_ITEMS = [
+  { label: "ORDERS", path: routes.order },
+  { label: "READY ORDERS", path: routes.readyOrder },
+];
